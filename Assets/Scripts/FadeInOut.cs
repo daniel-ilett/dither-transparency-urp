ae4c51620ec4e6cd0ffdfc4dcb65da541922bcaf
Daniel Ilett <danielilett@gmail.com>
2020-04-19 19:13:29 +0100

    Added example mesh and preview scripts.

--- Assets/Scripts/FadeInOut.cs
+++ Assets/Scripts/FadeInOut.cs
@@ -2,21 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Renderer))]
 public class FadeInOut : MonoBehaviour
 {
     [SerializeField] private float speed = 1.0f;
 
-    private new Renderer renderer;
-
-    private void Awake()
-    {
-        renderer = GetComponent<Renderer>();
-    }
+    [SerializeField] private Renderer[] renderers;
 
     private void Update()
     {
         var opacity = (Mathf.Sin(Time.time * speed) + 1.0f) / 2.0f;
-        renderer.material.SetFloat("_Opacity", opacity);
+
+        foreach(var renderer in renderers)
+        {
+            renderer.material.SetFloat("_Opacity", opacity);
+        }
     }
 }
---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    [SerializeField] private Transform orbitObject;
+    [SerializeField] private float orbitSize;
+    [SerializeField] private float orbitOffset;
+
+    private void Update()
+    {
+        var time = Time.time * Mathf.PI * 0.25f;
+
+        var xPos = orbitObject.position.x + Mathf.Sin(time) * orbitSize;
+        var yPos = orbitObject.position.y + orbitOffset;
+        var zPos = orbitObject.position.z + Mathf.Cos(time) * orbitSize;
+
+        transform.position = new Vector3(xPos, yPos, zPos);
+        transform.LookAt(orbitObject);
+    }
+}
